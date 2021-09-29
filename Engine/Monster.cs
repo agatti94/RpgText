3a96b51cb450d76cd90c243ac8397407ff412f86
ARTHUR GATTI <arthur.gatti@valorup.com.br>
2021-09-29 17:57:08 -0300

    First Commit

---
+++ Engine/Monster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public Monster(int id, string name, int maximumDamage, int rewardExperience, int rewardGold, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = MaximumDamage;
+            RewardExperiencePoints = RewardExperiencePoints;
+            RewardGold = rewardGold;
+        }
+    }
+}
---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,65 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            //instatiating a new player and defining his attributes
+            _player = new Player(10,10,20,0,1);
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            //setting the values of properties of the labels and trnsforming into a string
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
+            //instatiating a new location and setting attributes
+            Location test1 = new Location(1,"Home","This is your house");
+            Location test2 = new Location(1, "Your House", "This is your house", null, null, null);
+        }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+
+        private void lblGold_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
